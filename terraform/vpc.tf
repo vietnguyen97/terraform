dd4ef86ef732829707b0394dad0695bc20f65e64
vietnguyen97 <112857700+aiflocksvn@users.noreply.github.com>
2023-03-01 12:41:37 +0700

    update vpc #1

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -20,7 +20,7 @@ resource "aws_internet_gateway" "gw" {
 # 3. Create subnet
 resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.staging-vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = "10.0.0.0/26"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-southeast-1b"
   tags = {
@@ -30,7 +30,7 @@ resource "aws_subnet" "public-subnet-1" {
 
 resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.staging-vpc.id
-  cidr_block              = "10.0.2.0/24"
+  cidr_block              = "10.0.0.64/26"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-southeast-1c"
   tags = {
@@ -40,7 +40,7 @@ resource "aws_subnet" "public-subnet-2" {
 
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                  = aws_vpc.staging-vpc.id
-  cidr_block              = "10.0.3.0/24"
+  cidr_block              = "10.0.0.60/26"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-southeast-1a"
   tags = {
@@ -50,7 +50,7 @@ resource "aws_subnet" "private-subnet-1" {
  
 resource "aws_subnet" "private-subnet-2" {
   vpc_id                  = aws_vpc.staging-vpc.id
-  cidr_block              = "10.0.4.0/24"
+  cidr_block              = "10.0.0.61/26"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-southeast-1b"
   tags = {
