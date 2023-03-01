a5fd4990771c8605ea3720fcac7aea6b169edb9f
vietnguyen97 <112857700+aiflocksvn@users.noreply.github.com>
2023-03-01 14:57:24 +0700

    update vpc instanes

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,6 +1,12 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key-terraform"
+  public_key = file(var.PUB_KEY)
+}
+
 resource "aws_instance" "terraform-name" {
   ami                       = var.AMI[var.REGION]
   instance_type             = "t2.micro"
+  subnet_id                 = aws_subnet.public-subnet-1.id
   availability_zone         = var.REGION_SUB
   key_name                  = "deployer-key-terraform"
   vpc_security_group_ids    = ["sg-02d80deb4c72bb144"]   
