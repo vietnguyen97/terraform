de5e7d2e238af529a24fc7362ced861363baed81
vietnguyen97 <112857700+aiflocksvn@users.noreply.github.com>
2023-03-01 13:30:18 +0700

    fix update vpc #9

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -40,7 +40,7 @@ resource "aws_subnet" "public-subnet-2" {
 
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                  = aws_vpc.staging-vpc.id
-  cidr_block              = "10.0.0.62/26"
+  cidr_block              = "10.0.0.65/14"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-southeast-1a"
   tags = {
@@ -50,7 +50,7 @@ resource "aws_subnet" "private-subnet-1" {
  
 resource "aws_subnet" "private-subnet-2" {
   vpc_id                  = aws_vpc.staging-vpc.id
-  cidr_block              = "10.0.0.62/26"
+  cidr_block              = "10.0.0.78/14"
   map_public_ip_on_launch = "true"
   availability_zone       = "ap-southeast-1b"
   tags = {
